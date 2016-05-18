2e02fb5c1074270ce7da51a3e80d906a83d65941
Mario Kahlhofer <mario.ka@aon.at>
2016-05-18 14:08:34 +0200

    cg project inserted

---
+++ main.js
@@ -0,0 +1,214 @@
+/**
+ * Created by Clemens Birklbauer on 22.02.2016.
+ */
+'use strict';
+
+var gl = null;
+const camera = {
+  rotation: {
+    x: 0,
+    y: 0
+  }
+};
+
+//scene graph nodes
+var root = null;
+var rootnofloor = null;
+var translateLight;
+var rotateLight;
+var lightNode;
+var rotateNode;
+var shadowNode;
+
+var translate;
+var renderFloor;
+
+//textures
+var envcubetexture;
+var renderTargetColorTexture;
+var renderTargetDepthTexture;
+
+//framebuffer variables
+var renderTargetFramebuffer;
+var framebufferWidth = 1024;
+var framebufferHeight = 1024;
+
+var lightViewProjectionMatrix;
+
+//load the required resources using a utility function
+loadResources({
+  vs_shadow: 'shader/shadow.vs.glsl',
+  fs_shadow: 'shader/shadow.fs.glsl',
+  vs_single: 'shader/single.vs.glsl',
+  fs_single: 'shader/single.fs.glsl',
+  model: 'models/C-3PO.obj'
+}).then(function (resources /*an object containing our keys with the loaded resources*/) {
+  init(resources);
+
+  render(0);
+});
+
+function init(resources) {
+  //create a GL context
+  gl = createContext(400, 400);
+
+  gl.enable(gl.DEPTH_TEST);
+
+  //create scenegraph
+  root = createSceneGraph(gl, resources);
+
+  //create scenegraph without floor and simple shader
+  rootnofloor = new ShaderSGNode(createProgram(gl, resources.vs_single, resources.fs_single));
+  rootnofloor.append(rotateNode); //reuse model part
+
+  initInteraction(gl.canvas);
+}
+
+function createSceneGraph(gl, resources) {
+  //create scenegraph
+  const root = new ShaderSGNode(createProgram(gl, resources.vs_shadow, resources.fs_shadow));
+
+  //add node for setting shadow parameters
+  shadowNode = new ShadowSGNode(renderTargetDepthTexture,3,framebufferWidth,framebufferHeight);
+  root.append(shadowNode);
+
+  //light debug helper function
+  function createLightSphere() {
+    return new ShaderSGNode(createProgram(gl, resources.vs_single, resources.fs_single), [
+      new RenderSGNode(makeSphere(.2,10,10))
+    ]);
+  }
+
+  {
+    //initialize light
+    lightNode = new LightSGNode(); //use now framework implementation of light node
+    lightNode.ambient = [0.2, 0.2, 0.2, 1];
+    lightNode.diffuse = [0.8, 0.8, 0.8, 1];
+    lightNode.specular = [1, 1, 1, 1];
+    lightNode.position = [0, 0, 0];
+
+    rotateLight = new TransformationSGNode(mat4.create());
+    translateLight = new TransformationSGNode(glm.translate(0,-3,3)); //translating the light is the same as setting the light position
+
+    rotateLight.append(translateLight);
+    translateLight.append(lightNode);
+    translateLight.append(createLightSphere()); //add sphere for debugging: since we use 0,0,0 as our light position the sphere is at the same position as the light source
+    shadowNode.append(rotateLight);
+  }
+
+  {
+    //initialize C3PO
+    let c3po = new MaterialSGNode([ //use now framework implementation of material node
+      new RenderSGNode(resources.model)
+    ]);
+    //gold
+    c3po.ambient = [0.24725, 0.1995, 0.0745, 1];
+    c3po.diffuse = [0.75164, 0.60648, 0.22648, 1];
+    c3po.specular = [0.628281, 0.555802, 0.366065, 1];
+    c3po.shininess = 0.4;
+
+    rotateNode = new TransformationSGNode(mat4.create(), [
+      new TransformationSGNode(glm.transform({ translate: [0,1, 0], rotateX : 180, scale: 0.8 }),  [
+        c3po
+      ])
+    ]);
+    shadowNode.append(rotateNode);
+  }
+
+  {
+    //initialize floor
+    let floor = new MaterialSGNode(
+                new TextureSGNode(renderTargetDepthTexture,2,
+                new RenderSGNode(makeRect(2,2))
+                )
+              );
+
+    //dark
+    floor.ambient = [0, 0, 0, 1];
+    floor.diffuse = [0.1, 0.1, 0.1, 1];
+    floor.specular = [0.5, 0.5, 0.5, 1];
+    floor.shininess = 50.0;
+
+    shadowNode.append(new TransformationSGNode(glm.transform({ translate: [0,1,0], rotateX: 90, scale: 2}), [
+      floor
+    ]));
+  }
+
+  return root;
+}
+
+function render(timeInMilliseconds) {
+  checkForWindowResize(gl);
+
+  //update animations
+  //Note: We have to update all animations before generating the shadow map!
+  rotateNode.matrix = glm.rotateY(timeInMilliseconds*-0.01);
+  rotateLight.matrix = glm.rotateY(timeInMilliseconds*0.05);
+
+  //setup viewport
+  gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+  gl.clearColor(0.9, 0.9, 0.9, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  //setup context and camera matrices
+  const context = createSGContext(gl);
+  context.projectionMatrix = mat4.perspective(mat4.create(), 30, gl.drawingBufferWidth / gl.drawingBufferHeight, 0.01, 100);
+  //very primitive camera implementation
+  let lookAtMatrix = mat4.lookAt(mat4.create(), [0,-1,-4], [0,0,0], [0,1,0]);
+  let mouseRotateMatrix = mat4.multiply(mat4.create(),
+                          glm.rotateX(camera.rotation.y),
+                          glm.rotateY(camera.rotation.x));
+  context.viewMatrix = mat4.multiply(mat4.create(), lookAtMatrix, mouseRotateMatrix);
+
+
+  //get inverse view matrix to allow computing eye-to-light matrix
+  context.invViewMatrix = mat4.invert(mat4.create(), context.viewMatrix);
+
+  //render scenegraph
+  root.render(context);
+
+  //animate
+  requestAnimationFrame(render);
+}
+
+//camera control
+function initInteraction(canvas) {
+  const mouse = {
+    pos: { x : 0, y : 0},
+    leftButtonDown: false
+  };
+  function toPos(event) {
+    //convert to local coordinates
+    const rect = canvas.getBoundingClientRect();
+    return {
+      x: event.clientX - rect.left,
+      y: event.clientY - rect.top
+    };
+  }
+  canvas.addEventListener('mousedown', function(event) {
+    mouse.pos = toPos(event);
+    mouse.leftButtonDown = event.button === 0;
+  });
+  canvas.addEventListener('mousemove', function(event) {
+    const pos = toPos(event);
+    const delta = { x : mouse.pos.x - pos.x, y: mouse.pos.y - pos.y };
+    if (mouse.leftButtonDown) {
+      //add the relative movement of the mouse to the rotation variables
+  		camera.rotation.x += delta.x;
+  		camera.rotation.y += delta.y;
+    }
+    mouse.pos = pos;
+  });
+  canvas.addEventListener('mouseup', function(event) {
+    mouse.pos = toPos(event);
+    mouse.leftButtonDown = false;
+  });
+  //register globally
+  document.addEventListener('keypress', function(event) {
+    //https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
+    if (event.code === 'KeyR') {
+      camera.rotation.x = 0;
+  		camera.rotation.y = 0;
+    }
+  });
+}
---
+++ nodes/TextureSGNode.js
@@ -0,0 +1,31 @@
+//a scene graph node for setting texture parameters
+class TextureSGNode extends SGNode {
+  constructor(texture, textureunit, children ) {
+      super(children);
+      this.texture = texture;
+      this.textureunit = textureunit;
+  }
+
+  render(context)
+  {
+    //tell shader to use our texture
+    gl.uniform1i(gl.getUniformLocation(context.shader, 'u_enableObjectTexture'), 1);
+
+    //set additional shader parameters
+    gl.uniform1i(gl.getUniformLocation(context.shader, 'u_tex'), this.textureunit);
+
+    //activate and bind texture
+    gl.activeTexture(gl.TEXTURE0 + this.textureunit);
+    gl.bindTexture(gl.TEXTURE_2D, this.texture);
+
+    //render children
+    super.render(context);
+
+    //clean up
+    gl.activeTexture(gl.TEXTURE0 + this.textureunit);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+
+    //disable texturing in shader
+    gl.uniform1i(gl.getUniformLocation(context.shader, 'u_enableObjectTexture'), 0);
+  }
+}
