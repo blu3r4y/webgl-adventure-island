0941fcbf0b39f0eef66b611485aa19c7c6b28c26
christina <christina.r@outlook.at>
2016-06-09 10:35:57 +0200

    Texture problem

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var gl = null;
 const camera = {
   rotation: {
     x: 0,
-    y: 0
+    y: 75
   },
   pos: {
     x: 0,
@@ -25,8 +25,9 @@ var rootnofloor = null;
 var translateLight;
 var rotateLight;
 var lightNode;
-var rotateNode;
+var vehicleNode;
 var shadowNode;
+var pyramidNode;
 
 var translate;
 var renderFloor;
@@ -52,8 +53,11 @@ loadResources({
   vs_single: 'shader/single.vs.glsl',
   fs_single: 'shader/single.fs.glsl',
   fs_island: 'shader/island.fs.glsl',
+  vs_tex: 'shader/texture.vs.glsl',
+  fs_tex: 'shader/texture.fs.glsl',
   island: 'models/island.obj',
-  vehicle: 'models/vehicle.obj'
+  vehicle: 'models/vehicle.obj',
+  tree: 'models/tree2.png'
 }).then(function (resources /*an object containing our keys with the loaded resources*/) {
   init(resources);
 
@@ -71,41 +75,37 @@ function init(resources) {
 
   //create scenegraph without floor and simple shader
   //rootnofloor = new ShaderSGNode(createProgram(gl, resources.vs_single, resources.fs_single));
-  //rootnofloor.append(rotateNode); //reuse model part
+  //rootnofloor.append(vehicleNode); //reuse model part
 
   initInteraction(gl.canvas);
 }
 
 function createSceneGraph(gl, resources) {
   //create scenegraph
-//  const root = new ShaderSGNode(createProgram(gl, resources.vs_shadow, resources.fs_shadow));
   const root = new ShaderSGNode(createProgram(gl, resources.vs_shadow, resources.fs_shadow));
-  let islandsh = new ShaderSGNode(createProgram(gl, resources.vs_shadow, resources.fs_island));
-  let island = new RenderSGNode(resources.island);
-  islandsh.append(island);
+  pyramidNode = new TransformationSGNode(mat4.create(), [new TransformationSGNode(glm.transform({ translate: [0,0,0.5], scale: 0.5 }),  [ new RenderSGNode(makePyramid())])]);
   let vehicle = new MaterialSGNode([ //use now framework implementation of material node
-    new RenderSGNode(resources.vehicle)
+    new RenderSGNode(makeVehicle()),
+    pyramidNode
   ]);
+  //vehicle.append(new RenderSGNode(makePyramid()));
   //gold
-  vehicle.ambient = [0.24725, 0.1995, 0.0745, 1];
-  vehicle.diffuse = [0.75164, 0.60648, 0.22648, 1];
-  vehicle.specular = [0.628281, 0.555802, 0.366065, 1];
+  vehicle.ambient = [0.24725, 0.1995, 0.2745, 1];
+  vehicle.diffuse = [0.75164, 0.60648, 0.42648, 1];
+  vehicle.specular = [0.628281, 0.555802, 0.666065, 1];
   vehicle.shininess = 0.4;
-  root.append(islandsh);
+  let island = new RenderSGNode(resources.island);
+  root.append(island);
 
-  let rotateNode = new TransformationSGNode(mat4.create(), [
-      new TransformationSGNode(glm.transform({ translate: [1,1,0.1], rotateX : 0, scale: 0.5 }),  [
-       vehicle
-      ])
-    ]);
-  root.append(rotateNode);
+  let billboard = new TransformationSGNode(mat4.create(), [new TransformationSGNode(glm.transform({ translate: [-2,-2,0.5], scale: 0.25 }),  [new ShaderSGNode(createProgram(gl, resources.vs_tex, resources.fs_tex), [new MaterialSGNode([new AdvancedTextureSGNode(resources.tree, [new RenderSGNode(makeBillboard())])])])])]);
+  root.append(billboard);
   //add node for setting shadow parameters
   //initialize light
   let light = new LightSGNode(); //use now framework implementation of light node
   light.ambient = [0.2, 0.2, 0.2, 1];
   light.diffuse = [0.8, 0.8, 0.8, 1];
   light.specular = [1, 1, 1, 1];
-  light.position = [0, 0, 0];
+  light.position = [0,-0.5,0.25];
 
   function createLightSphere() {
       return new ShaderSGNode(createProgram(gl, resources.vs_single, resources.fs_single), [
@@ -114,14 +114,65 @@ function createSceneGraph(gl, resources) {
     }
 
   rotateLight = new TransformationSGNode(mat4.create());
-  let translateLight = new TransformationSGNode(glm.translate(0,-2,2)); //translating the light is the same as setting the light position
-
-  rotateLight.append(translateLight);
+//  let translateLight = new TransformationSGNode(glm.translate(0,-2,2)); //translating the light is the same as setting the light position
+  let translateLight =  new TransformationSGNode(glm.translate(0,-0.5,0.25));
+  let lightSphere = new ShaderSGNode(createProgram(gl, resources.vs_single, resources.fs_single), [new RenderSGNode(makeSphere(.1,10,10)) ]);
+//  rotateLight.append(translateLight);
   translateLight.append(light);
-  translateLight.append(createLightSphere()); //add sphere for debugging: since we use 0,0,0 as our light position the sphere is at the same position as the light source
-  root.append(rotateLight);
-
+//  translateLight.append(createLightSphere()); //add sphere for debugging: since we use 0,0,0 as our light position the sphere is at the same position as the light source
+  translateLight.append(lightSphere);
+  //root.append(rotateLight);
+  vehicleNode = new TransformationSGNode(mat4.create(), [
+      new TransformationSGNode(glm.transform({ translate: [0,0,0.1], scale: 0.5 }),  [
+      // vehicle, new TransformationSGNode(glm.translate(0,-0.5,0.25), [new ShaderSGNode(createProgram(gl, resources.vs_single, resources.fs_single), [new RenderSGNode(makeSphere(.1,10,10)) ])])
+      vehicle, translateLight
+    //  new TransformationSGNode(glm.translate(1,1,10.25) [new RenderSGNode(makePyramid())])
+      //new MaterialSGNode([new RenderSGNode(makePyramid())])
+      ])
+    ]);
+  root.append(vehicleNode);
   return root;
+}
+
+function makeVehicle() {
+  var position = [-0.5, 0.5, 0.5, 0.5, 0.5, 0.5, -0.25, -0.5, 0.5, 0.25, -0.5, 0.5, -0.5, 0.5, 0.0,
+  0.5, 0.5, 0.0, -0.25, -0.5, 0.0, 0.25, -0.5, 0.0];
+  var normal = [-1, -1, -1,   1, -1, -1,   -1, 1, -1,   1, 1, -1,  -1, -1, 1,   1, -1, 1,   -1, 1, 1,   1, 1, 1];
+  var texturecoordinates = [0, 0,   5, 0,   5, 5,   0, 5,  5, 0,   5, 5,   0, 0, 0, 0];
+  var index = [0, 1, 2,  1, 2, 3,  4, 5, 6,  5, 6, 7, 0, 1, 4, 1, 4, 5, 0, 2, 4, 2, 4, 6, 2, 3, 6, 3, 6, 7, 1, 3, 5, 3, 5, 7];
+  return {
+    position: position,
+    normal: normal,
+    texture: null,
+    index: index
+  };
+}
+
+function makePyramid() {
+  var position = [0, 0, 0.3, 0, 0.3, 0, -0.3, -0.3, 0, 0.3, -0.3, 0];
+  var normal = [0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1];
+  var index = [0, 1, 2, 0, 2, 3, 0, 3, 1];
+  return {
+    position: position,
+    normal: null,
+    texture: null,
+    index: index
+  };
+}
+
+function makeBillboard() {
+  var width = 2;
+  var height = 2;
+  var position = [-width, 0, -height,   width, 0, -height,    width, 0, height,   -width, 0, height, ];
+  var normal = [0, 0, 1,   0, 0, 1,   0, 0, 1,   0, 0, 1];
+  var texturecoordinates = [1, 1,   0, 1,   0, 0,   1, 0];
+  var index = [0, 1, 2,  2, 3, 0];
+  return {
+    position: position,
+    normal: normal,
+    texture: texturecoordinates,
+    index: index
+  };
 }
 
 function render(timeInMilliseconds) {
@@ -129,7 +180,7 @@ function render(timeInMilliseconds) {
 
   //update animations
   //Note: We have to update all animations before generating the shadow map!
-  //rotateNode.matrix = glm.rotateY(timeInMilliseconds*-0.01);
+  //vehicleNode.matrix = glm.rotateY(timeInMilliseconds*-0.01);
   //rotateLight.matrix = glm.rotateY(timeInMilliseconds*0.05);
 
   //setup viewport
@@ -149,6 +200,7 @@ function render(timeInMilliseconds) {
   if(camera.pos.z > 0 && !userControlled){
       camera.pos.z = camera.pos.z - zoom;
   }
+  pyramidNode.matrix = glm.rotateZ(timeInMilliseconds*-0.01);
   //get inverse view matrix to allow computing eye-to-light matrix
   context.invViewMatrix = mat4.invert(mat4.create(), context.viewMatrix);
 
